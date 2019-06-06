c662afafe6b39773c97a513bb9f34970c8505895
panltc <39292075+panltc@users.noreply.github.com>
2019-06-06 20:39:17 +0800

    1 approach

--- 0007_Reverse Integer.java
+++ 0007_Reverse Integer.java
@@ -2,11 +2,11 @@ class Solution {
     public int reverse(int x) {
         int res = 0;
         while (x != 0) {
-            int cur = res * 10 + x % 10;
-            if (cur / 10 != res) {
+            int tmp = res * 10 + x % 10;
+            if (tmp / 10 != res) {
                 return 0;
             }
-            res = cur;
+            res = tmp;
             x /= 10;
         }
         return res;
