abde336b794a58b824b0c6f3224172c6aa37c9d4
panltc <39292075+panltc@users.noreply.github.com>
2019-06-06 21:02:23 +0800

    1 approach

--- 0008_String to Integer (atoi).java
+++ 0008_String to Integer (atoi).java
@@ -1,11 +1,11 @@
 class Solution {
-    public static int myAtoi(String str) {
+    public int myAtoi(String str) {
         int N = str.length();
-        int sign = 1;
         int i = 0;
         while (i < N && str.charAt(i) == ' ') {
             i++;
         }
+        int sign = 1;
         if (i < N && (str.charAt(i) == '-' || str.charAt(i) == '+')) {
             sign = str.charAt(i++) == '-' ? -1 : 1;
         }
@@ -13,12 +13,11 @@ class Solution {
         int MAX_REMAINDER = Integer.MAX_VALUE % 10;
         int res = 0;
         while (i < N && (str.charAt(i) >= '0' && str.charAt(i) <= '9')) {
-            int cur = str.charAt(i) - '0';
+            int cur = str.charAt(i++) - '0';
             if (res > MAX_QUOTIENT || (res == MAX_QUOTIENT && cur > MAX_REMAINDER)) {
                 return sign == -1 ? Integer.MIN_VALUE : Integer.MAX_VALUE;
             }
             res = res * 10 + cur;
-            i++;
         }
         return res * sign;
     }
