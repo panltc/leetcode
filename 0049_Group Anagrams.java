3e8838cd905d102076d8335593691b3a1efa21be
panltc <39292075+panltc@users.noreply.github.com>
2019-06-07 16:10:38 +0800

    1 approach

--- 0049_Group Anagrams.java
+++ 0049_Group Anagrams.java
@@ -2,9 +2,9 @@ class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         Map<String, List<String>> map = new HashMap<>();
         for (String str : strs) {
-            char[] tmp = str.toCharArray();
-            Arrays.sort(tmp);
-            String key = String.valueOf(tmp);
+            char[] data = str.toCharArray();
+            Arrays.sort(data);
+            String key = String.valueOf(data);
             if (!map.containsKey(key)) {
                 map.put(key, new ArrayList<>());
             }
