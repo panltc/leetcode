13b76d71cd2fe000ec829cbcf172730f17c62b60
panltc <39292075+panltc@users.noreply.github.com>
2019-06-07 19:38:46 +0800

    1 approach

--- 0125_Valid Palindrome.java
+++ 0125_Valid Palindrome.java
@@ -1,10 +1,16 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        s = s.replaceAll("[^0-9A-Za-z]", "").toLowerCase();
-        int i = 0;
-        int j = s.length() - 1;
-        while (i < j) {
-            if (s.charAt(i++) != s.charAt(j--)) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if ((c >= '0' && c <= '9') || (c >= 'a' && c <= 'z')) {
+                sb.append(c);
+            } else if (c >= 'A' && c <= 'Z') {
+                sb.append((char)(c - 'A' + 'a'));
+            }
+        }
+        for (int i = 0, j = sb.length() - 1; i < j; i++, j--) {
+            if (sb.charAt(i) != sb.charAt(j)) {
                 return false;
             }
         }
