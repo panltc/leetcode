531a64eec389c4c3862cc7fee1215359f11edb13
panltc <39292075+panltc@users.noreply.github.com>
2019-06-07 20:10:51 +0800

    2 approaches

--- 0141_Linked List Cycle.java
+++ 0141_Linked List Cycle.java
@@ -1,17 +1,29 @@
+// Approach 1
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        if (head == null || head.next == null) {
-            return false;
-        }
-        ListNode fast = head.next;
+        ListNode fast = head;
         ListNode slow = head;
-        while (fast != slow) {
-            if (fast == null || fast.next == null) {
-                return false;
-            }
+        while (fast != null && fast.next != null) {
             fast = fast.next.next;
             slow = slow.next;
+            if (fast == slow) {
+                return true;
+            }
         }
-        return true;
+        return false;
+    }
+}
+
+// Approach 2
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        Set<ListNode> set = new HashSet<>();
+        while (head != null) {
+            head = head.next;
+            if (!set.add(head)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
