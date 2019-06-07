054d770e50efffcc96680ec1eebc6dc30c11957d
panltc <39292075+panltc@users.noreply.github.com>
2019-06-07 23:37:11 +0800

    1 approach

--- 0977_Squares of a Sorted Array.java
+++ 0977_Squares of a Sorted Array.java
@@ -3,13 +3,7 @@ class Solution {
         int N = A.length;
         int[] res = new int[N];
         for (int i = 0, j = N - 1, k = N - 1; k >= 0; k--) {
-            if (Math.abs(A[i]) < Math.abs(A[j])) {
-                res[k] = A[j] * A[j];
-                j--;
-            } else {
-                res[k] = A[i] * A[i];
-                i++;
-            }
+            res[k] = Math.abs(A[i]) < Math.abs(A[j]) ? A[j] * A[j--] : A[i] * A[i++];
         }
         return res;
     }
