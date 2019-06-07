d4dab53344b27cc66e383ee7e445a0522ccd7af3
panltc <39292075+panltc@users.noreply.github.com>
2019-06-07 22:18:28 +0800

    1 approach

--- 1051_Height Checker.java
+++ 1051_Height Checker.java
@@ -1,11 +1,11 @@
 class Solution {
     public int heightChecker(int[] heights) {
         int N = heights.length;
-        int[] tmp = Arrays.copyOf(heights, N);
-        Arrays.sort(tmp);
+        int[] data = Arrays.copyOf(heights, N);
+        Arrays.sort(data);
         int res = 0;
         for (int i = 0; i < N; i++) {
-            if (tmp[i] != heights[i]) {
+            if (data[i] != heights[i]) {
                 res++;
             }
         }
