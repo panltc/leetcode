fdd1129c313b30e29e991ebc82001e8160b27988
panltc <39292075+panltc@users.noreply.github.com>
2019-06-07 23:33:36 +0800

    1 approach

--- 0994_Rotting Oranges.java
+++ 0994_Rotting Oranges.java
@@ -1,9 +1,11 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
+        int M = grid.length;
+        int N = grid[0].length;
         Queue<int[]> queue = new LinkedList<>();
         int cnt = 0;
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
+        for (int i = 0; i < M; i++) {
+            for (int j = 0; j < N; j++) {
                 if (grid[i][j] == 1) {
                     cnt++;
                 } else if (grid[i][j] == 2) {
@@ -22,7 +24,7 @@ class Solution {
                 for (int[] dir : dirs) {
                     int x = cur[0] + dir[0];
                     int y = cur[1] + dir[1];
-                    if (x < 0 || x >= grid.length || y < 0 || y >= grid[0].length || grid[x][y] != 1) {
+                    if (x < 0 || x == M || y < 0 || y == N || grid[x][y] != 1) {
                         continue;
                     }
                     grid[x][y] = 2;
