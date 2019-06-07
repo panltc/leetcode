d89a5bb1ec7284363e5ba1135293724b475298ba
panltc <39292075+panltc@users.noreply.github.com>
2019-06-07 09:16:30 +0800

    1 approach

--- 0018_4Sum.java
+++ 0018_4Sum.java
@@ -4,10 +4,22 @@ class Solution {
         Arrays.sort(nums);
         List<List<Integer>> res = new ArrayList<>();
         for (int i = 0; i < N - 3; i++) {
+            if (nums[i] + nums[i + 1] + nums[i + 2] + nums[i + 3] > target) {
+                break;
+            }
+            if (nums[i] + nums[N - 3] + nums[N - 2] + nums[N - 1] < target) {
+                continue;
+            }
             if (i > 0 && nums[i] == nums[i - 1]) {
                 continue;
             }
             for (int j = i + 1; j < N - 2; j++) {
+                if (nums[i] + nums[j] + nums[j + 1] + nums[j + 2] > target) {
+                    break;
+                }
+                if (nums[i] + nums[j] + nums[N - 2] + nums[N - 1] < target) {
+                    continue;
+                }
                 if (j > i + 1 && nums[j] == nums[j - 1]) {
                     continue;
                 }
