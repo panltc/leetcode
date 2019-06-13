925189d70a58041b4c38f7dd3ef85888c6562240
panltc <39292075+panltc@users.noreply.github.com>
2019-06-13 10:42:54 +0800

    1 approach

---
+++ 0257_Binary Tree Paths.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<String> binaryTreePaths(TreeNode root) {
+        List<String> res = new ArrayList<>();
+        binaryTreePaths(root, "", res);
+        return res;
+    }
+    
+    private void binaryTreePaths(TreeNode root, String cur, List<String> res) {
+        if (root == null) {
+            return;
+        }
+        if (root.left == null && root.right == null) {
+            res.add(cur + root.val);
+            return;
+        }
+        binaryTreePaths(root.left, cur + root.val + "->" , res);
+        binaryTreePaths(root.right, cur + root.val + "->" , res);
+    }
+}
