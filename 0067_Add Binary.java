11370a8e736f73fa41cc88a6e16d42bd9b5cedf8
panltc <39292075+panltc@users.noreply.github.com>
2019-06-13 23:41:36 +0800

    1 approach

--- 0067_Add Binary.java
+++ 0067_Add Binary.java
@@ -1,9 +1,9 @@
 class Solution {
     public String addBinary(String a, String b) {
         StringBuilder sb = new StringBuilder();
-        int sum = 0;
         int i = a.length() - 1;
         int j = b.length() - 1;
+        int sum = 0;
         while (i >= 0 || j >= 0 || sum != 0) {
             if (i >= 0) {
                 sum += a.charAt(i--) - '0';
