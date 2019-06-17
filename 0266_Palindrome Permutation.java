78e3081ce96158f18f4d825f904f46c26d6fa514
panltc <39292075+panltc@users.noreply.github.com>
2019-06-17 14:31:09 +0800

    1 approach

---
+++ 0266_Palindrome Permutation.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean canPermutePalindrome(String s) {
+        int[] cnts = new int[128];
+        int odd = 0;
+        for (char c :s.toCharArray()) {
+            cnts[c]++;
+            odd += cnts[c] % 2 == 0 ? -1 : 1;
+        }
+        return odd <= 1;
+    }
+}
