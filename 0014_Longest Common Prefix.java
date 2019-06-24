92399ae93c0e0255a172fabe7ebbe789ac9993d8
panltc <39292075+panltc@users.noreply.github.com>
2019-06-24 23:19:37 +0800

    1 approach

--- 0014_Longest Common Prefix.java
+++ 0014_Longest Common Prefix.java
@@ -5,13 +5,13 @@ class Solution {
         }
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < strs[0].length(); i++) {
-            char c = strs[0].charAt(i);
+            char cur = strs[0].charAt(i);
             for (String str : strs) {
-                if (str.length() <= i || str.charAt(i) != c) {
+                if (str.length() <= i || str.charAt(i) != cur) {
                     return sb.toString();
                 }
             }
-            sb.append(c);
+            sb.append(cur);
         }
         return sb.toString();
     }
