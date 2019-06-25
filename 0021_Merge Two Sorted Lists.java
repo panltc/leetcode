98b767f5d4834c86752ffd8cfc67aafc99ce1973
panltc <39292075+panltc@users.noreply.github.com>
2019-06-25 14:03:38 +0800

    2 approaches

--- 0021_Merge Two Sorted Lists.java
+++ 0021_Merge Two Sorted Lists.java
@@ -24,6 +24,9 @@ class Solution {
 // Approach 2: Recursive
 class Solution {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null && l2 == null) {
+            return null;
+        }
         if (l1 == null) {
             return l2;
         }
