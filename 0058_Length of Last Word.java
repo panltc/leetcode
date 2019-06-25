c91e9782e58a87caee39605c7e678bf59ef50f07
panltc <39292075+panltc@users.noreply.github.com>
2019-06-26 00:05:50 +0800

    1 approach

--- 0058_Length of Last Word.java
+++ 0058_Length of Last Word.java
@@ -1,10 +1,10 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        int res = 0;
         int i = s.length() - 1;
         while (i >= 0 && s.charAt(i) == ' ') {
             i--;
         }
+        int res = 0;
         while (i >= 0 && s.charAt(i) != ' ') {
             res++;
             i--;
