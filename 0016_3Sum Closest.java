884e5561c935ab7e4b179b4197f738cd9cca2bd1
panltc <39292075+panltc@users.noreply.github.com>
2019-06-25 10:01:44 +0800

    1 approach

--- 0016_3Sum Closest.java
+++ 0016_3Sum Closest.java
@@ -4,9 +4,6 @@ class Solution {
         Arrays.sort(nums);
         int res = nums[0] + nums[1] + nums[2];
         for (int i = 0; i < N - 2; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) {
-                continue;
-            }
             int j = i + 1;
             int k = N - 1;
             while (j < k) {
