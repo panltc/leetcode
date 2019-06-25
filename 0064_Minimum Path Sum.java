7d63e5eee8d68b176bf719c97daae472091afc3e
panltc <39292075+panltc@users.noreply.github.com>
2019-06-26 00:58:16 +0800

    1 approach

---
+++ 0064_Minimum Path Sum.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int minPathSum(int[][] grid) {
+        int M = grid.length;
+        int N = grid[0].length;
+        int[][] dp = new int[M][N];
+        for (int i = 0; i < M; i++) {
+            for (int j = 0; j < N; j++) {
+                if (i == 0 && j == 0) {
+                    dp[i][j] = grid[i][j];
+                } else if (i == 0) {
+                    dp[i][j] = dp[i][j - 1] + grid[i][j];
+                } else if (j == 0) {
+                    dp[i][j] = dp[i - 1][j] + grid[i][j];
+                } else {
+                    dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+                }
+            }
+        }
+        return dp[M - 1][N - 1];
+    }
+}
