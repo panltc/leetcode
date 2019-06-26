0f2362e188a5cbb43487b99d4bd395c57df60b82
panltc <39292075+panltc@users.noreply.github.com>
2019-06-26 14:49:01 +0800

    1 approach

--- 0022_Generate Parentheses.java
+++ 0022_Generate Parentheses.java
@@ -4,20 +4,20 @@ class Solution {
         if (n == 0) {
             return res;
         }
-        backtrack(n, 0, 0, "", res);
+        dfs(n, 0, 0, "", res);
         return res;
     }
     
-    private void backtrack(int n, int i, int j, String cur, List<String> res) {
+    private void dfs(int n, int i, int j, String cur, List<String> res) {
         if (j == n) {
             res.add(cur);
             return;
         }
         if (i < n) {
-            backtrack(n, i + 1, j, cur + "(", res);
+            dfs(n, i + 1, j, cur + "(", res);
         }
         if (j < i) {
-            backtrack(n, i, j + 1, cur + ")", res);
+            dfs(n, i, j + 1, cur + ")", res);
         }
     }
 }
