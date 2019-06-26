addea2a54e6fdb997c02c36d984a0bd53faaf6b5
panltc <39292075+panltc@users.noreply.github.com>
2019-06-26 15:07:28 +0800

    2 approaches

--- 0083_Remove Duplicates from Sorted List.java
+++ 0083_Remove Duplicates from Sorted List.java
@@ -1,4 +1,4 @@
-// Approach 1: Iterative
+// approach 1: iterative
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
         ListNode cur = head;
@@ -13,7 +13,7 @@ class Solution {
     }
 }
 
-// Approach 2: Recursive
+// approach 2: recursive
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
         if (head == null || head.next == null) {
