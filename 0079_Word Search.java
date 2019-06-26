b4f2e3dce2f6372f63746d7f1a5ba30d6c3aed86
panltc <39292075+panltc@users.noreply.github.com>
2019-06-26 14:47:05 +0800

    1 approach

--- 0079_Word Search.java
+++ 0079_Word Search.java
@@ -2,7 +2,7 @@ class Solution {
     public boolean exist(char[][] board, String word) {
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[0].length; j++) {
-                if (backtrack(board, i, j, word, 0)) {
+                if (dfs(board, i, j, word, 0)) {
                     return true;
                 }
             }
@@ -10,7 +10,7 @@ class Solution {
         return false;
     }
     
-    private boolean backtrack(char[][] board, int i, int j, String word, int k) {
+    private boolean dfs(char[][] board, int i, int j, String word, int k) {
         if (k == word.length()) {
             return true;
         }
@@ -18,10 +18,10 @@ class Solution {
             return false;
         }
         board[i][j] = '#';
-        if (backtrack(board, i - 1, j, word, k + 1) || 
-            backtrack(board, i + 1, j, word, k + 1) || 
-            backtrack(board, i, j - 1, word, k + 1) ||
-            backtrack(board, i, j + 1, word, k + 1)) {
+        if (dfs(board, i - 1, j, word, k + 1) || 
+            dfs(board, i + 1, j, word, k + 1) || 
+            dfs(board, i, j - 1, word, k + 1) || 
+            dfs(board, i, j + 1, word, k + 1)) {
             return true;
         }
         board[i][j] = word.charAt(k);
