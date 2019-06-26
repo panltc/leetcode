ab01abe31936ed0790064c5fcc33756a18af3e87
panltc <39292075+panltc@users.noreply.github.com>
2019-06-26 15:28:01 +0800

    2 approaches

--- 0094_Binary Tree Inorder Traversal.java
+++ 0094_Binary Tree Inorder Traversal.java
@@ -1,17 +1,36 @@
+// Approach 1: Iterative
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
         Stack<TreeNode> stack = new Stack<>();
         List<Integer> res = new ArrayList<>();
         while (!stack.isEmpty() || root != null) {
-            if (root != null) {
-                stack.push(root);
-                root = root.left;
-            } else {
+            if (root == null) {
                 root = stack.pop();
                 res.add(root.val);
                 root = root.right;
+            } else {
+                stack.push(root);
+                root = root.left;
             }
         }
         return res;
+    }
+}
+
+// Approach 2: Recursive
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        inorderTraversal(root, res);
+        return res;
+    }
+    
+    private void inorderTraversal(TreeNode root, List<Integer> res) {
+        if (root == null) {
+            return;
+        }
+        inorderTraversal(root.left, res);
+        res.add(root.val);
+        inorderTraversal(root.right, res);
     }
 }
