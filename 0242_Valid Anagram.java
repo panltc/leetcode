f74825aa3666351c13ad4f87554e592ab65dd90b
panltc <39292075+panltc@users.noreply.github.com>
2019-06-28 00:07:54 +0800

    2 approaches

--- 0242_Valid Anagram.java
+++ 0242_Valid Anagram.java
@@ -1,13 +1,11 @@
-// solution 1
+// Approach 1
 class Solution {
     public boolean isAnagram(String s, String t) {
-        int S = s.length();
-        int T = t.length();
-        if (S != T) {
+        if (s.length() != t.length()) {
             return false;
         }
         int[] cnts = new int[26];
-        for (int i = 0; i < S; i++) {
+        for (int i = 0; i < s.length(); i++) {
             cnts[s.charAt(i) - 'a']++;
             cnts[t.charAt(i) - 'a']--;
         }
@@ -20,20 +18,18 @@ class Solution {
     }
 }
 
-// solution 2
+// Approach 2
 class Solution {
     public boolean isAnagram(String s, String t) {
-        int S = s.length();
-        int T = s.length();
-        if (S != T) {
+        if (s.length() != t.length()) {
             return false;
         }
         int[] cnts = new int[26];
-        for (int i = 0; i < S; i++) {
-            cnts[s.charAt(i) - 'a']++;
+        for (char c : s.toCharArray()) {
+            cnts[c - 'a']++;
         }
-        for (int i = 0; i < T; i++) {
-            if (cnts[t.charAt(i) - 'a']-- == 0) {
+        for (char c : t.toCharArray()) {
+            if (cnts[c - 'a']-- == 0) {
                 return false;
             }
         }
