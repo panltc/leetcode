917f78fd58b44e418bed777bfb61e162ba65608b
panltc <39292075+panltc@users.noreply.github.com>
2019-06-27 14:56:22 +0800

    1 approach

--- 0167_Two Sum II - Input array is sorted.java
+++ 0167_Two Sum II - Input array is sorted.java
@@ -4,12 +4,13 @@ class Solution {
         int j = numbers.length - 1;
         while (i < j) {
             int sum = numbers[i] + numbers[j];
+            if (sum == target) {
+                return new int[] {i + 1, j + 1};
+            }
             if (sum < target) {
                 i++;
-            } else if (sum > target) {
-                j--;
             } else {
-                return new int[] {i + 1, j + 1};
+                j--;
             }
         }
         return null;
