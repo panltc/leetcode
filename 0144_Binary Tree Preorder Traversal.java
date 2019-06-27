a178f0f06f05eba992911ff0ee30858d8a62c212
panltc <39292075+panltc@users.noreply.github.com>
2019-06-27 14:01:08 +0800

    2 approaches

--- 0144_Binary Tree Preorder Traversal.java
+++ 0144_Binary Tree Preorder Traversal.java
@@ -1,16 +1,35 @@
+// Approach 1: Iterative
 class Solution {
     public List<Integer> preorderTraversal(TreeNode root) {
         Stack<TreeNode> stack = new Stack<>();
         List<Integer> res = new ArrayList<>();
         while (!stack.isEmpty() || root != null) {
-            if (root != null) {
-                stack.push(root);
-                res.add(root.val);
-                root = root.left;
-            } else {
+            if (root == null) {
                 root = stack.pop().right;
+            } else {
+                res.add(root.val);
+                stack.push(root);
+                root = root.left;
             }
         }
         return res;
+    }
+}
+
+// Approach 2: Recursive
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        preorderTraversal(root, res);
+        return res;
+    }
+    
+    private void preorderTraversal(TreeNode root, List<Integer> res) {
+        if (root == null) {
+            return;
+        }
+        res.add(root.val);
+        preorderTraversal(root.left, res);
+        preorderTraversal(root.right, res);
     }
 }
