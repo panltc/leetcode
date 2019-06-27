d2ba7dfc05fe58af33c4045384193b0cfd5116b0
panltc <39292075+panltc@users.noreply.github.com>
2019-06-27 15:05:02 +0800

    1 approach

--- 0169_Majority Element.java
+++ 0169_Majority Element.java
@@ -3,11 +3,11 @@ class Solution {
         int res = 0;
         int cnt = 0;
         for (int num : nums) {
-            if (cnt != 0) {
-                cnt += res == num ? 1 : -1;
-            } else {
+            if (cnt == 0) {
                 res = num;
                 cnt = 1;
+            } else {
+                cnt += num == res ? 1 : -1;
             }
         }
         return res;
