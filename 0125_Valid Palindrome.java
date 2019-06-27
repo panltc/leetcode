850cba29a20191a8ec9054cd86acd06801bdae83
panltc <39292075+panltc@users.noreply.github.com>
2019-06-27 10:17:18 +0800

    1 approach

--- 0125_Valid Palindrome.java
+++ 0125_Valid Palindrome.java
@@ -1,11 +1,10 @@
 class Solution {
     public boolean isPalindrome(String s) {
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
+        for (char c : s.toCharArray()) {
             if ((c >= '0' && c <= '9') || (c >= 'a' && c <= 'z')) {
                 sb.append(c);
-            } else if (c >= 'A' && c <= 'Z') {
+            } else if (c >= 'A' && c <='Z') {
                 sb.append((char)(c - 'A' + 'a'));
             }
         }
