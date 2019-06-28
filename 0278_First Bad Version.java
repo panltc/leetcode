d9e3c33f4060c5998a9788e3bcf9f8c020d11167
panltc <39292075+panltc@users.noreply.github.com>
2019-06-28 10:08:37 +0800

    1 approach

--- 0278_First Bad Version.java
+++ 0278_First Bad Version.java
@@ -4,10 +4,10 @@ public class Solution extends VersionControl {
         int high = n;
         while (low < high) {
             int mid = low + (high - low) / 2;
-            if (!isBadVersion(mid)) {
-                low = mid + 1;
-            } else {
+            if (isBadVersion(mid)) {
                 high = mid;
+            } else {
+                low = mid + 1;
             }
         }
         return low;
