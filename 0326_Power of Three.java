df071dd7f5d877b1307fc9a858e4e0d3373effa2
panltc <39292075+panltc@users.noreply.github.com>
2019-06-28 13:06:20 +0800

    2 approaches

--- 0326_Power of Three.java
+++ 0326_Power of Three.java
@@ -1,11 +1,11 @@
-// solution 1
+// Approach 1
 class Solution {
     public boolean isPowerOfThree(int n) {
         return (n > 0) && (1162261467 % n == 0);
     }
 }
 
-// solution 2
+// Approach 2
 class Solution {
     public boolean isPowerOfThree(int n) {
         return (Math.log10(n) / Math.log10(3)) % 1 == 0;
