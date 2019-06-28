fc55b2818f0055dc4b88b0f9df3db905a3d58321
panltc <39292075+panltc@users.noreply.github.com>
2019-06-28 15:53:20 +0800

    2 approaches

--- 0461_Hamming Distance.java
+++ 0461_Hamming Distance.java
@@ -1,24 +1,24 @@
-// solution 1
+// Approach 1
 class Solution {
     public int hammingDistance(int x, int y) {
-        int i = x ^ y;
+        int data = x ^ y;
         int res = 0;
-        while (i != 0) {
+        while (data != 0) {
             res++;
-            i &= i - 1;
+            data &= data - 1;
         }
         return res;
     }
 }
 
-// solution 2
+// Approach 2
 class Solution {
     public int hammingDistance(int x, int y) {
-        int i = x ^ y;
+        int data = x ^ y;
         int res = 0;
-        while (i != 0) {
-            res += i & 1;
-            i >>>= 1;
+        while (data != 0) {
+            res += data & 1;
+            data >>>= 1;
         }
         return res;
     }
