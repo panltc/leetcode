bafedaefff8839cfdf70c5185396702d69105a7c
panltc <39292075+panltc@users.noreply.github.com>
2019-06-28 20:14:44 +0800

    1 approach

---
+++ 0804_Unique Morse Code Words.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int uniqueMorseRepresentations(String[] words) {
+        String[] data = {".-", "-...", "-.-.", "-..", ".", 
+                         "..-.", "--.", "....", "..", ".---", 
+                         "-.-", ".-..", "--", "-.", "---", 
+                         ".--.", "--.-", ".-.", "...", "-", 
+                         "..-", "...-", ".--", "-..-", "-.--", 
+                         "--.."};
+        Set<String> set = new HashSet<>();
+        for (String word : words) {
+            StringBuilder sb = new StringBuilder();
+            for (char c : word.toCharArray()) {
+                sb.append(data[c - 'a']);
+            }
+            set.add(sb.toString());
+        }
+        return set.size();
+    }
+}
