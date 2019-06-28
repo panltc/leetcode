9d4bf16baadff20516704ad6ace3b9907d6de667
panltc <39292075+panltc@users.noreply.github.com>
2019-06-28 18:49:58 +0800

    1 approach

--- 0682_Baseball Game.java
+++ 0682_Baseball Game.java
@@ -1,16 +1,17 @@
 class Solution {
     public int calPoints(String[] ops) {
-        Stack<Integer> stack = new Stack<>();
         int res = 0;
+        Stack<Integer> stack = new Stack<>();
         for (String op : ops) {
             if (op.equals("+")) {
                 int x = stack.pop();
                 int y = stack.peek();
                 stack.push(x);
                 stack.push(x + y);
-                res += x + y;
+                res += stack.peek();
             } else if (op.equals("D")) {
-                res += stack.push(stack.peek() * 2);
+                stack.push(stack.peek() * 2);
+                res += stack.peek();
             } else if (op.equals("C")) {
                 res -= stack.pop();
             } else {
