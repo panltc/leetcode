9b33b7fc7a24c5dccca504b257441275059022f0
panltc <39292075+panltc@users.noreply.github.com>
2019-06-28 19:23:25 +0800

    1 approach

--- 0747_Largest Number At Least Twice of Others.java
+++ 0747_Largest Number At Least Twice of Others.java
@@ -1,8 +1,8 @@
 class Solution {
     public int dominantIndex(int[] nums) {
         int res = 0;
-        int max = 0;
-        int sm = 0;
+        int max = Integer.MIN_VALUE;
+        int sm = Integer.MIN_VALUE;
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] > max) {
                 sm = max;
