f7de34528036760b1d5405bcdad08bde66d95792
panltc <39292075+panltc@users.noreply.github.com>
2019-06-28 19:01:13 +0800

    1 approach

--- 0704_Binary Search.java
+++ 0704_Binary Search.java
@@ -4,12 +4,13 @@ class Solution {
         int high = nums.length - 1;
         while (low <= high) {
             int mid = low + (high - low) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
             if (nums[mid] < target) {
                 low = mid + 1;
-            } else if (nums[mid] > target) {
-                high = mid - 1;
             } else {
-                return mid;
+                high = mid - 1;
             }
         }
         return -1;
