7a1bb9b0b1612412c0b7585779e2371750d53585
panltc <39292075+panltc@users.noreply.github.com>
2019-06-28 13:11:24 +0800

    1 approach

--- 0328_Odd Even Linked List.java
+++ 0328_Odd Even Linked List.java
@@ -1,36 +1,15 @@
-// solution 1
 class Solution {
     public ListNode oddEvenList(ListNode head) {
-        if (head == null) {
-            return null;
+        if (head == null || head.next == null) {
+            return head;
         }
         ListNode odd = head;
         ListNode even = head.next;
         ListNode tmp = even;
-        while (even != null && even.next != null) {
+        while (odd.next != null && even.next != null) {
             odd.next = odd.next.next;
             odd = odd.next;
             even.next = even.next.next;
-            even = even.next;
-        }
-        odd.next = tmp;
-        return head;
-    }
-}
-
-// solution 2
-class Solution {
-    public ListNode oddEvenList(ListNode head) {
-        if (head == null) {
-            return null;
-        }
-        ListNode odd = head;
-        ListNode even = head.next;
-        ListNode tmp = even;
-        while (even != null && even.next != null) {
-            odd.next = even.next;
-            odd = odd.next;
-            even.next = odd.next;
             even = even.next;
         }
         odd.next = tmp;
