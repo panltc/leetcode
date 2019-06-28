8012aa96dc60d255fc774fc196266296ea8487e9
panltc <39292075+panltc@users.noreply.github.com>
2019-06-28 14:44:58 +0800

    2 approaches

--- 0371_Sum of Two Integers.java
+++ 0371_Sum of Two Integers.java
@@ -1,16 +1,16 @@
-// solution 1 (iterative)
+// Approach 1: iterative
 class Solution {
     public int getSum(int a, int b) {
         while (b != 0) {
-            int carry = (a & b) << 1;
+            int tmp = (a & b) << 1;
             a ^= b;
-            b = carry;
+            b = tmp;
         }
         return a;
     }
 }
 
-// solution 2 (recursive)
+// Approach 2: recursive
 class Solution {
     public int getSum(int a, int b) {
         return b == 0 ? a : getSum(a ^ b, (a & b) << 1);
