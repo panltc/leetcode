1aee2b945e3c4ac7ed08e993d06c3f353408ff23
panltc <39292075+panltc@users.noreply.github.com>
2019-06-28 14:47:30 +0800

    1 approach

--- 0374_Guess Number Higher or Lower.java
+++ 0374_Guess Number Higher or Lower.java
@@ -4,12 +4,13 @@ public class Solution extends GuessGame {
         int high = n;
         while (low <= high) {
             int mid = low + (high - low) / 2;
+            if (guess(mid) == 0) {
+                return mid;
+            }
             if (guess(mid) == -1) {
                 high = mid - 1;
-            } else if (guess(mid) == 1) {
-                low = mid + 1;
             } else {
-                return mid;
+                low = mid + 1;
             }
         }
         return 0;
