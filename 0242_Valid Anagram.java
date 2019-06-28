074f841468f2f1e58edc7cff9397f20e40f3b849
panltc <39292075+panltc@users.noreply.github.com>
2019-06-28 15:16:25 +0800

    1 approach

--- 0242_Valid Anagram.java
+++ 0242_Valid Anagram.java
@@ -1,4 +1,3 @@
-// Approach 1
 class Solution {
     public boolean isAnagram(String s, String t) {
         if (s.length() != t.length()) {
@@ -11,25 +10,6 @@ class Solution {
         }
         for (int cnt : cnts) {
             if (cnt != 0) {
-                return false;
-            }
-        }
-        return true;
-    }
-}
-
-// Approach 2
-class Solution {
-    public boolean isAnagram(String s, String t) {
-        if (s.length() != t.length()) {
-            return false;
-        }
-        int[] cnts = new int[26];
-        for (char c : s.toCharArray()) {
-            cnts[c - 'a']++;
-        }
-        for (char c : t.toCharArray()) {
-            if (cnts[c - 'a']-- == 0) {
                 return false;
             }
         }
