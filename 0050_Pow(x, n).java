3704e2d29fa839a5cb38b33e5e8b6da94346c531
panltc <39292075+panltc@users.noreply.github.com>
2019-06-30 19:55:23 +0800

    1 approach

--- 0050_Pow(x, n).java
+++ 0050_Pow(x, n).java
@@ -1,12 +1,14 @@
 class Solution {
     public double myPow(double x, int n) {
         double res = 1.0;
-        for (int i = n; i != 0; i /= 2) {
-            if (i % 2 != 0) {
+        int tmp = n;
+        while (n != 0) {
+            if (n % 2 != 0) {
                 res *= x;
             }
             x *= x;
+            n /= 2;
         }
-        return n < 0 ? 1 / res : res;
+        return tmp < 0 ? 1 / res : res;
     }
 }
