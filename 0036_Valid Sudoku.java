0d21b44ef9c27628397fe60b5325e472e8ec124a
panltc <39292075+panltc@users.noreply.github.com>
2019-07-01 13:46:43 +0800

    1 approach

--- 0036_Valid Sudoku.java
+++ 0036_Valid Sudoku.java
@@ -5,7 +5,9 @@ class Solution {
             for (int j = 0; j < 9; j++) {
                 char c = board[i][j];
                 if (c != '.') {
-                    if (!set.add(i + "r: " + c) || !set.add(j + "c: " + c) || !set.add(i / 3 + ", " + j / 3 + "b: " + c)) {
+                    if (!set.add(i + "r: " + c) || 
+                        !set.add(j + "c: " + c) || 
+                        !set.add(i / 3 + ", " + j / 3 + "b: " + c)) {
                         return false;
                     }
                 }
