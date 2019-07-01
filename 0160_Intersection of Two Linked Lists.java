712c47235147b2cc8e0d645ce42a0f0398edade5
panltc <39292075+panltc@users.noreply.github.com>
2019-07-01 19:19:19 +0800

    1 approach

--- 0160_Intersection of Two Linked Lists.java
+++ 0160_Intersection of Two Linked Lists.java
@@ -1,8 +1,5 @@
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        if (headA == null || headB == null) {
-            return null;
-        }
         ListNode curA = headA;
         ListNode curB = headB;
         while (curA != curB) {
