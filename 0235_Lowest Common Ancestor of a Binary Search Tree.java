8b6bd534a4c67fbc2b0b5d75f87dca62cb1f1bbd
panltc <39292075+panltc@users.noreply.github.com>
2019-07-20 22:03:27 +0800

    2 approaches

--- 0235_Lowest Common Ancestor of a Binary Search Tree.java
+++ 0235_Lowest Common Ancestor of a Binary Search Tree.java
@@ -1,3 +1,24 @@
+// Approach 1: iterative
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            TreeNode cur = queue.poll();
+            if ((p.val <= cur.val && q.val >= cur.val) || (p.val >= cur.val && q.val <= cur.val)) {
+                return cur;
+            }
+            if (p.val < cur.val) {
+                queue.offer(cur.left);
+            } else {
+                queue.offer(cur.right);
+            }
+        }
+        return null;
+    }
+}
+
+// Approach 2: recursive
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         if (p.val < root.val && q.val < root.val) {
