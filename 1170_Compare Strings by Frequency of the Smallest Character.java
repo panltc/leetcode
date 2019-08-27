9a07ec6494d7bb037e8c0c2b58acc49c4d2304ba
panltc <39292075+panltc@users.noreply.github.com>
2019-08-27 15:50:00 +0800

    1 approach

---
+++ 1170_Compare Strings by Frequency of the Smallest Character.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] numSmallerByFrequency(String[] queries, String[] words) {
+        int[] cnts = new int[11];
+        for (String word : words) {
+            cnts[frequency(word)]++;
+        }
+        int N = queries.length;
+        int[] res = new int[N];
+        for (int i = 0; i < N; i++) {
+            for (int j = frequency(queries[i]) + 1; j <= 10; j++) {
+                res[i] += cnts[j];
+            }
+        }
+        return res;
+    }
+    
+    private int frequency(String str) {
+        int[] cnts = new int[26];
+        int min = 25;
+        for (char c : str.toCharArray()) {
+            min = Math.min(c - 'a', min);
+            cnts[c - 'a']++;
+        }
+        return cnts[min];
+    }
+}
