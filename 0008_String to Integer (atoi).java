d15513d95b0b996dd561a2d1a26bc54a1212873e
panltc <39292075+panltc@users.noreply.github.com>
2019-09-06 14:34:25 +0800

    1 approach

--- 0008_String to Integer (atoi).java
+++ 0008_String to Integer (atoi).java
@@ -1,23 +1,23 @@
 class Solution {
     public int myAtoi(String str) {
-        int N = str.length();
+        int len = str.length();
         int i = 0;
-        while (i < N && str.charAt(i) == ' ') {
+        while (i < len && str.charAt(i) == ' ') {
             i++;
         }
         int sign = 1;
-        if (i < N && (str.charAt(i) == '-' || str.charAt(i) == '+')) {
+        if (i < len && (str.charAt(i) == '-' || str.charAt(i) == '+')) {
             sign = str.charAt(i++) == '-' ? -1 : 1;
         }
         int MAX_QUOTIENT = Integer.MAX_VALUE / 10;
         int MAX_REMAINDER = Integer.MAX_VALUE % 10;
         int res = 0;
-        while (i < N && (str.charAt(i) >= '0' && str.charAt(i) <= '9')) {
-            int cur = str.charAt(i++) - '0';
-            if (res > MAX_QUOTIENT || (res == MAX_QUOTIENT && cur > MAX_REMAINDER)) {
+        while (i < len && (str.charAt(i) >= '0' && str.charAt(i) <= '9')) {
+            int data = str.charAt(i++) - '0';
+            if (res > MAX_QUOTIENT || (res == MAX_QUOTIENT && data > MAX_REMAINDER)) {
                 return sign == -1 ? Integer.MIN_VALUE : Integer.MAX_VALUE;
             }
-            res = res * 10 + cur;
+            res = res * 10 + data;
         }
         return res * sign;
     }
