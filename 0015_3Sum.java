73469a1e9ade5bd47aa2a9be7f3650fd79158484
panltc <39292075+panltc@users.noreply.github.com>
2019-09-11 13:03:27 +0800

    1 approach

--- 0015_3Sum.java
+++ 0015_3Sum.java
@@ -1,20 +1,20 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        int N = nums.length;
+        int len = nums.length;
         Arrays.sort(nums);
         List<List<Integer>> res = new ArrayList<>();
-        for (int i = 0; i < N - 2 && nums[i] <= 0; i++) {
+        for (int i = 0; i < len - 2 && nums[i] <= 0; i++) {
             if (nums[i] + nums[i + 1] + nums[i + 2] > 0) {
                 break;
             }
-            if (nums[i] + nums[N - 2] + nums[N - 1] < 0) {
+            if (nums[i] + nums[len - 2] + nums[len - 1] < 0) {
                 continue;
             }
             if (i > 0 && nums[i] == nums[i - 1]) {
                 continue;
             }
             int j = i + 1;
-            int k = N - 1;
+            int k = len - 1;
             while (j < k) {
                 int sum = nums[i] + nums[j] + nums[k];
                 if (sum == 0) {
