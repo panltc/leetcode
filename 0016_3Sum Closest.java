5a025c943a7d0cb28fc239c0f5a9b786ef5356e4
panltc <39292075+panltc@users.noreply.github.com>
2019-09-11 13:11:59 +0800

    1 approach

--- 0016_3Sum Closest.java
+++ 0016_3Sum Closest.java
@@ -1,11 +1,11 @@
 class Solution {
     public int threeSumClosest(int[] nums, int target) {
-        int N = nums.length;
+        int len = nums.length;
         Arrays.sort(nums);
         int res = nums[0] + nums[1] + nums[2];
-        for (int i = 0; i < N - 2; i++) {
+        for (int i = 0; i < len - 2; i++) {
             int j = i + 1;
-            int k = N - 1;
+            int k = len - 1;
             while (j < k) {
                 int sum = nums[i] + nums[j] + nums[k];
                 if (sum == target) {
