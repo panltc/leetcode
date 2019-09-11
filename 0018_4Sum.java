0d3c642e3c4cbb33cfe268f3695bff3b6fd3133e
panltc <39292075+panltc@users.noreply.github.com>
2019-09-11 14:30:27 +0800

    1 approach

--- 0018_4Sum.java
+++ 0018_4Sum.java
@@ -1,30 +1,30 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        int N = nums.length;
+        int len = nums.length;
         Arrays.sort(nums);
         List<List<Integer>> res = new ArrayList<>();
-        for (int i = 0; i < N - 3; i++) {
+        for (int i = 0; i < len - 3; i++) {
             if (nums[i] + nums[i + 1] + nums[i + 2] + nums[i + 3] > target) {
                 break;
             }
-            if (nums[i] + nums[N - 3] + nums[N - 2] + nums[N - 1] < target) {
+            if (nums[i] + nums[len - 3] + nums[len - 2] + nums[len - 1] < target) {
                 continue;
             }
             if (i > 0 && nums[i] == nums[i - 1]) {
                 continue;
             }
-            for (int j = i + 1; j < N - 2; j++) {
+            for (int j = i + 1; j < len - 2; j++) {
                 if (nums[i] + nums[j] + nums[j + 1] + nums[j + 2] > target) {
                     break;
                 }
-                if (nums[i] + nums[j] + nums[N - 2] + nums[N - 1] < target) {
+                if (nums[i] + nums[j] + nums[len - 2] + nums[len - 1] < target) {
                     continue;
                 }
                 if (j > i + 1 && nums[j] == nums[j - 1]) {
                     continue;
                 }
                 int k = j + 1;
-                int l = N - 1;
+                int l = len - 1;
                 while (k < l) {
                     int sum = nums[i] + nums[j] + nums[k] + nums[l];
                     if (sum == target) {
