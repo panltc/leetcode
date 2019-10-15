37b5df81fc7bd01dcb8b1a600d6bafede40de1e1
panltc <39292075+panltc@users.noreply.github.com>
2019-10-15 11:26:08 +0800

    1 approach

--- 0700_Search in a Binary Search Tree.java
+++ 0700_Search in a Binary Search Tree.java
@@ -1,9 +1,6 @@
 class Solution {
     public TreeNode searchBST(TreeNode root, int val) {
-        if (root == null) {
-            return null;
-        }
-        if (root.val == val) {
+        if (root == null || root.val == val) {
             return root;
         }
         return root.val < val ? searchBST(root.right, val) : searchBST(root.left, val);
