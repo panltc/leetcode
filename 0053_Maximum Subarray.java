46b9049f25090e43060ca98933c5539f3be7571d
panltc <39292075+panltc@users.noreply.github.com>
2021-03-25 13:33:41 +0800

    1 approach

--- 0053_Maximum Subarray.java
+++ 0053_Maximum Subarray.java
@@ -1,11 +1,20 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        int cur = 0;
-        int max = Integer.MIN_VALUE;
+        int res = Integer.MIN_VALUE;
+        // curr代表已有最大子序和
+        int curr = 0;
+        
+        /*
+         * 动态规划方程：f(n) = max(f(n − 1) + nums[i], nums[i])
+         * f(n)：以下标n结尾的子序列的最大和
+         */
         for (int num : nums) {
-            cur = Math.max(num, cur + num);
-            max = Math.max(cur, max);
+            // 若已有最大子序和加num后大于num，则num应该加入子序列中，否则，以当前元素重新作为子序列的开始
+            curr = Math.max(curr + num, num);
+            // 对每个元素进行操作后都会得到一个以当前元素结尾或开始的子序列的最大和，与已有的最大子序和进行比较，取最大值
+            res = Math.max(res, curr);
         }
-        return max;
+
+        return res;
     }
 }
