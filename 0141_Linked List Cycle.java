939b68f3c348d8c7e75f9166bfdca958d5252431
panltc <39292075+panltc@users.noreply.github.com>
2021-03-26 15:20:16 +0800

    2 approaches

--- 0141_Linked List Cycle.java
+++ 0141_Linked List Cycle.java
@@ -3,13 +3,17 @@ public class Solution {
     public boolean hasCycle(ListNode head) {
         ListNode fast = head;
         ListNode slow = head;
+        
+        // 快指针跑得快，所以只需要考虑快指针的边界条件
         while (fast != null && fast.next != null) {
             fast = fast.next.next;
             slow = slow.next;
+            // 如果快慢指针相遇了，说明肯定存在环
             if (fast == slow) {
                 return true;
             }
         }
+        
         return false;
     }
 }
@@ -17,13 +21,17 @@ public class Solution {
 // Approach 2
 public class Solution {
     public boolean hasCycle(ListNode head) {
+        // set数据结构可以保证节点的唯一性
         Set<ListNode> set = new HashSet<>();
+
         while (head != null) {
+            // 对于遍历到的每一个节点，都将其加入set。如果一个节点不能加入set，说明早就遍历过该元素了，从而证明链表中存在环
             if (!set.add(head)) {
                 return true;
             }
             head = head.next;
         }
+
         return false;
     }
 }
