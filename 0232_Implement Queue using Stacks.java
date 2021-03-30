be374b61a51d194c1922ffad95641c298d8dcc71
panltc <39292075+panltc@users.noreply.github.com>
2021-03-30 10:18:35 +0800

    1 approach

--- 0232_Implement Queue using Stacks.java
+++ 0232_Implement Queue using Stacks.java
@@ -1,4 +1,5 @@
 class MyQueue {
+    // stack1是输入栈，stack2是输出栈
     private Stack<Integer> stack1;
     private Stack<Integer> stack2;
 
@@ -9,17 +10,20 @@ class MyQueue {
     }
     
     /** Push element x to the back of queue. */
+    // 压入新元素的时候，都先存入stack1中
     public void push(int x) {
         stack1.push(x);
     }
     
     /** Removes the element from in front of queue and returns that element. */
+    // 在stack2空的时候，把stack1中全部元素放入stack2中
     public int pop() {
         if (stack2.isEmpty()) {
             while (!stack1.isEmpty()) {
                 stack2.push(stack1.pop());
             }
         }
+
         return stack2.pop();
     }
     
@@ -30,10 +34,12 @@ class MyQueue {
                 stack2.push(stack1.pop());
             }
         }
+
         return stack2.peek();
     }
     
     /** Returns whether the queue is empty. */
+    // 如果两个栈都空，才说明队列空了
     public boolean empty() {
         return stack1.isEmpty() && stack2.isEmpty();
     }
