f4c22d36c540a15583a429826595cadd904ae03b
panltc <39292075+panltc@users.noreply.github.com>
2021-03-31 15:47:53 +0800

    1 approach

--- 0046_Permutations.java
+++ 0046_Permutations.java
@@ -1,28 +1,39 @@
 class Solution {
+    private List<List<Integer>> res = new ArrayList<>();
+    // visited数组用来记录当前递归栈中，哪个元素被访问了
+    private boolean[] visited;
+
     public List<List<Integer>> permute(int[] nums) {
-        int N = nums.length;
-        List<List<Integer>> res = new ArrayList<>();
-        if (N == 0) {
+        int len = nums.length;
+
+        if (len == 0) {
             return res;
         }
-        boolean[] visited = new boolean[N];
-        backtrack(nums, visited, 0, new ArrayList<>(), res);
+        visited = new boolean[len];
+        dfs(nums, new ArrayList<>(), visited);
+
         return res;
     }
     
-    private void backtrack(int[] nums, boolean[] visited, int i, List<Integer> list, List<List<Integer>> res) {
-        int N = nums.length;
-        if (i == N) {
+    private void dfs(int[] nums, List<Integer> list, boolean[] visited) {
+        // 如果list的元素个数足够了，就将其加入到res结果集中（list只是一个引用，所以需要复制一份新的list加入res）
+        if (list.size() == nums.length) {
             res.add(new ArrayList<>(list));
+            
             return;
         }
-        for (int j = 0; j < N; j++) {
-            if (!visited[j]) {
-                list.add(nums[j]);
-                visited[j] = true;
-                backtrack(nums, visited, i + 1, list, res);
-                visited[j] = false;
+        // 每次递归都遍历所有的元素，通过visisted数组来判断是否访问过该元素
+        for (int i = 0; i < nums.length; i++) {
+            /* 如果没有访问过当前元素，说明可以将其加入list中
+             * 先将其置为true，代表访问了，然后加入list，进行下一层递归（每次递归都相当于固定了一个位置）
+             * dfs返回只有一种可能，就是当前list已经满了，回退一个固定的元素，并将visited置为false
+             */
+            if (!visited[i]) {
+                visited[i] = true;
+                list.add(nums[i]);
+                dfs(nums, list, visited);
                 list.remove(list.size() - 1);
+                visited[i] = false;
             }
         }
     }
