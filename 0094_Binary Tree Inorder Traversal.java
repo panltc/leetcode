9a0852d87d9f0e780dee39b8946ddd6de69cb544
panltc <39292075+panltc@users.noreply.github.com>
2021-04-03 23:03:04 +0800

    2 approaches

--- 0094_Binary Tree Inorder Traversal.java
+++ 0094_Binary Tree Inorder Traversal.java
@@ -3,34 +3,36 @@ class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
         List<Integer> res = new ArrayList<>();
         Stack<TreeNode> stack = new Stack<>();
+
+        // 首先如果root不为空，才会将其加入栈中。其他情况下，只需要判断栈是否为空来确定是否遍历完了二叉树
         while (!stack.isEmpty() || root != null) {
-            if (root == null) {
-                root = stack.pop();
-                res.add(root.val);
-                root = root.right;
-            } else {
+            // 只要root不为空，就将当前结点压入栈中，并遍历左子树
+            while (root != null) {
                 stack.push(root);
                 root = root.left;
             }
+            // 如果root为空，就弹出一个结点，并加入结果集，然后遍历右子树
+            root = stack.pop();
+            res.add(root.val);
+            root = root.right;
         }
+
         return res;
     }
 }
 
 // Approach 2: recursive
 class Solution {
+    private List<Integer> res = new ArrayList<>();
+
     public List<Integer> inorderTraversal(TreeNode root) {
-        List<Integer> res = new ArrayList<>();
-        inorderTraversal(root, res);
-        return res;
-    }
-    
-    private void inorderTraversal(TreeNode root, List<Integer> res) {
         if (root == null) {
-            return;
+            return res;
         }
-        inorderTraversal(root.left, res);
+        inorderTraversal(root.left);
         res.add(root.val);
-        inorderTraversal(root.right, res);
+        inorderTraversal(root.right);
+
+        return res;
     }
 }
