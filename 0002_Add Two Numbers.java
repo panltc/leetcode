fd1c9ef2cf0433159f1931ac5acc5a68e7347ab2
panltc <39292075+panltc@users.noreply.github.com>
2021-04-06 09:36:38 +0800

    1 approach

--- 0002_Add Two Numbers.java
+++ 0002_Add Two Numbers.java
@@ -1,8 +1,10 @@
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode dummy = new ListNode(0);
-        ListNode cur = dummy;
+        ListNode curr = dummy;
         int sum = 0;
+        
+        // 两个链表不一定等长，所以判断条件为或。同时，即使两个链表都遍历完毕，若存在一个进位，仍需进行操作。
         while (l1 != null || l2 != null || sum != 0) {
             if (l1 != null) {
                 sum += l1.val;
@@ -12,10 +14,13 @@ class Solution {
                 sum += l2.val;
                 l2 = l2.next;
             }
-            cur.next = new ListNode(sum % 10);
-            cur = cur.next;
+            // sum取模操作可以得出单独的个位
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
+            // 若sum > 10，则说明存在进位，除以10后变为1，代表进位，会累加到下次运算中。若sum < 10，则说明没有进位，除以10后变为0，不影响下次运算
             sum /= 10;
         }
+
         return dummy.next;
     }
 }
