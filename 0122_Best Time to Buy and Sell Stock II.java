88d814c0fa70ca0d32ddf921c3d80a43fad3b758
panltc <39292075+panltc@users.noreply.github.com>
2021-04-06 09:11:49 +0800

    1 approach

--- 0122_Best Time to Buy and Sell Stock II.java
+++ 0122_Best Time to Buy and Sell Stock II.java
@@ -1,11 +1,14 @@
 class Solution {
     public int maxProfit(int[] prices) {
         int res = 0;
+
+        // 只要第二天的价格大于第一天，就进行一次买卖
         for (int i = 1; i < prices.length; i++) {
             if (prices[i] > prices[i - 1]) {
                 res += prices[i] - prices[i - 1];
             }
         }
+
         return res;
     }
 }
