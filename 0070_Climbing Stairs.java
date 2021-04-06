6f678b4478a15b1d97bfb00171a12b7d6e06de18
panltc <39292075+panltc@users.noreply.github.com>
2021-04-06 08:26:54 +0800

    1 approach

--- 0070_Climbing Stairs.java
+++ 0070_Climbing Stairs.java
@@ -1,15 +1,23 @@
 class Solution {
     public int climbStairs(int n) {
+        // 爬一个台阶只有一种方法
         if (n == 1) {
             return 1;
         }
-        int pre = 1;
-        int cur = 2;
+
+        int prev = 1;
+        int res = 2;
+        
+        /* 动态规划方程：f(n) = f(n - 1) + f(n - 2)
+         * f(n)：爬n个台阶的方法。
+         * prev相当于f(n - 2)
+         */
         for (int i = 3; i <= n; i++) {
-            int tmp = cur;
-            cur += pre;
-            pre = tmp;
+            int tmp = res;
+            res += prev;
+            prev = tmp;
         }
-        return cur;
+
+        return res;
     }
 }
