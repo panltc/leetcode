815cec3c8d82ecbee2b4ba31bdc504c0ecb11f6d
panltc <39292075+panltc@users.noreply.github.com>
2021-04-07 09:01:52 +0800

    1 approach

--- 0113_Path Sum II.java
+++ 0113_Path Sum II.java
@@ -11,6 +11,10 @@ class Solution {
     }
 
     private void dfs(TreeNode root, int targetSum, List<Integer> list) {
+        // 若为空结点，返回
+        if (root == null) {
+            return;
+        }
         // 如果该结点是叶结点，并且当前targetSum减去当前结点的值后为0，说明找到了一条路径，先将该结点加入list，并将其加入结果集，最后仍需要删除新加入的结点
         if (root.left == null && root.right == null && targetSum - root.val == 0) {
             list.add(root.val);
@@ -19,16 +23,10 @@ class Solution {
 
             return;
         }
-        // 若为空结点，返回
-        if (root == null) {
-            return;
-        }
-        // 先将当前结点加入list中，并且targetSum减去当前结点的值（每次递归先处理当前结点）
         list.add(root.val);
-        targetSum -= root.val;
-        // 如果当前递归函数没有返回，说明当前结点不是空结点，也不是符合条件的叶结点。假设该结点为路径的一部分，深度遍历左子树和右子树
-        dfs(root.left, targetSum, list);
-        dfs(root.right, targetSum, list);
+        // 假设该结点为路径的一部分，targetSum要减去本层结点的值（每层处理每层targetSum的值），并深度遍历左子树和右子树
+        dfs(root.left, targetSum - root.val, list);
+        dfs(root.right, targetSum - root.val, list);
         // 回溯，将本次递归添加的该结点移除
         list.remove(list.size() - 1);
     }
