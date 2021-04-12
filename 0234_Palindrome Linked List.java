e6f4972c5c0c9349d04365f0022c786662e674d1
panltc <39292075+panltc@users.noreply.github.com>
2021-04-12 19:32:42 +0800

    1 approach

--- 0234_Palindrome Linked List.java
+++ 0234_Palindrome Linked List.java
@@ -1,29 +1,40 @@
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        ListNode fast = head;
-        ListNode slow = head;
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode fast = dummy;
+        ListNode slow = dummy;
+
+        // 奇数个结点，slow指针会停留在正中间。偶数个结点，slow会停留在链表a的尾部
         while (fast != null && fast.next != null) {
             fast = fast.next.next;
             slow = slow.next;
         }
-        if (fast != null) {
-            slow = slow.next;
+
+        // 链表a从head开始，链表b从slow下一个结点开始
+        ListNode currA = head;
+        ListNode currB = slow.next;
+        slow.next = null;
+        // 头结点重置，准备进行反转链表操作
+        dummy.next = null;
+
+        // 反转b链表
+        while (currB != null) {
+            ListNode tmp = currB.next;
+            currB.next = dummy.next;
+            dummy.next = currB;
+            currB = tmp;
         }
-        ListNode dummy = new ListNode(0);
-        while (slow != null) {
-            ListNode tmp = slow.next;
-            slow.next = dummy.next;
-            dummy.next = slow;
-            slow = tmp;
-        }
-        ListNode cur = dummy.next;
-        while (cur != null) {
-            if (cur.val != head.val) {
+
+        currB = dummy.next;
+        while (currA != null && currB != null) {
+            if (currA.val != currB.val) {
                 return false;
             }
-            cur = cur.next;
-            head = head.next;
+            currA = currA.next;
+            currB = currB.next;
         }
+
         return true;
     }
 }
