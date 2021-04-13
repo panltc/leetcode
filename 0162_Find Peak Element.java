89b204a6209aa237fa78bee3fe39ad9c8d0a5cf6
panltc <39292075+panltc@users.noreply.github.com>
2021-04-13 08:46:21 +0800

    1 approach

--- 0162_Find Peak Element.java
+++ 0162_Find Peak Element.java
@@ -2,6 +2,8 @@ class Solution {
     public int findPeakElement(int[] nums) {
         int low = 0;
         int high = nums.length - 1;
+        
+        // 如果nums[mid] < nums[mid + 1]，假设nums[mid + 1] > nums[mid + 1]，那mid + 1就是峰值位置，若不是，则继续后推，因为nums[n]为负无穷，所以mid + 1 ~ n必然存在峰值
         while (low < high) {
             int mid = low + (high - low) / 2;
             if (nums[mid] < nums[mid + 1]) {
@@ -10,6 +12,7 @@ class Solution {
                 high = mid;
             }
         }
+
         return low;
     }
 }
