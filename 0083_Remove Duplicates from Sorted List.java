cdca126f35fbdd1a765812c1ccbddd5c8e8870d0
panltc <39292075+panltc@users.noreply.github.com>
2021-04-14 09:51:58 +0800

    2 approaches

--- 0083_Remove Duplicates from Sorted List.java
+++ 0083_Remove Duplicates from Sorted List.java
@@ -1,14 +1,17 @@
 // Approach 1: iterative
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
-        ListNode cur = head;
-        while (cur != null && cur.next != null) {
-            if (cur.val == cur.next.val) {
-                cur.next = cur.next.next;
+        ListNode curr = head;
+
+        while (curr != null && curr.next != null) {
+            // 如果当前结点的值和下一个结点的值相同，将当前结点连接到下一个结点的下一个结点即可，否则，跳过当前结点
+            if (curr.val == curr.next.val) {
+                curr.next = curr.next.next;
             } else {
-                cur = cur.next;
+                curr = curr.next;
             }
         }
+
         return head;
     }
 }
@@ -16,10 +19,16 @@ class Solution {
 // Approach 2: recursive
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
+        // 如果当前结点为空，或者当前结点为最后一个结点，直接返回该结点
         if (head == null || head.next == null) {
             return head;
         }
+        // 递归调用该函数，参数为当前结点的下一个结点，并赋值给当前结点的next
         head.next = deleteDuplicates(head.next);
+
+        /* 如果当前结点的值和下一个结点的值相同，那么就跳过当前结点，返回下一个结点，否则返回当前结点
+         * 其实当前结点是连接着下一个结点开始的后续链表的，只是这里不返回以当前结点为开头的链表而已
+         */
         return head.val == head.next.val ? head.next : head;
     }
 }
